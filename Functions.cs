1da70e3f7da89c76db10e6fce1d40ebcac963edb
Govert van Drimmelen <govert@dnakode.com>
2023-04-19 16:29:50 +0200

    Add project files.

---
+++ Functions.cs
@@ -0,0 +1,17 @@
+﻿using ExcelDna.Integration;
+using System.Reflection;
+using System.Runtime.Loader;
+
+namespace TestDisableAssemblyContextUnload
+{
+    public static class Functions
+    {
+        public static object SayHello() => $"Hello from {ExcelDnaUtil.XllPathInfo.Name}!";
+
+        public static object IsCollectible()
+        {
+            var ass = Assembly.GetExecutingAssembly();
+            return $"{ass.FullName} is collectible: {AssemblyLoadContext.GetLoadContext(ass)!.IsCollectible}";
+        }
+    }
+}
